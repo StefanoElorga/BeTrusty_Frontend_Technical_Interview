dc8602d698d1046b83688f58cd761ce83795fbed
Stefano Elorga <stefanoelorga22@gmail.com>
2024-05-09 14:01:55 -0300

    nextjsapp y componente header

---
+++ app/betrusty/src/app/components/Header.tsx
@@ -0,0 +1,12 @@
+import logo from "../assets/Logo.png";
+import avatar from "../assets/Avatar.png";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <div className="bg-black w-full px-6 py-4 flex flex-row justify-between">
+      <Image src={logo} alt="logo" className="w-32 h-9" />
+      <Image src={avatar} alt="avatar" className="w-11 h-11" />
+    </div>
+  );
+}
