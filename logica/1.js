dc74c9c0d1dff7a5b5198ef0b627ef2aa5326905
Stefano Elorga <stefanoelorga22@gmail.com>
2024-05-10 19:31:49 -0300

    ejercicio logica: 1

--- logica/1.js
+++ logica/1.js
@@ -17,6 +17,41 @@
 // {car: 's', veces: 1}
 // {car: 'y', veces: 2}
 
-const contarCar = value => {
-  return
-}
+const contarCar = (value) => {
+  //Quitamos espacios y convertimos a minusculas
+  const stringWithoutSpaces = value.replace(/\s/g, "").toLowerCase();
+
+  //sacamos el acento a las letras que lo llevan
+  const charWithoutAccent = stringWithoutSpaces
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+
+  //separamos en un array cada caracter y los ordenamos alfabeticamente.
+  const orderChar = charWithoutAccent.split("").sort();
+
+  //creamos un objeto
+  const object = {};
+
+  //usando forEach iteramos el array ordenado, y le damos condiciones. (si ya existe el char en nuestro objeto solo sumamos, si no existe lo agregamos)
+  orderChar.forEach((char) => {
+    if (object[char]) {
+      object[char]++;
+    } else {
+      object[char] = 1;
+    }
+  });
+
+  //creamos un array que será nuestro resultado final.
+  const ourResult = [];
+
+  //usando for iteramos el objeto.
+  for (const char in object) {
+    ourResult.push({ car: char, veces: object[char] });
+  }
+
+  console.log(ourResult);
+
+  return ourResult;
+};
+
+contarCar("Hoy ya es día 10");
--- logica/2.js
+++ logica/2.js
@@ -12,6 +12,41 @@
       Salida: MOUREDEV
  */
 
-const t9Keyboard = value => {
-  return
-}
+const t9Keyboard = (value) => {
+  const keyboard = {
+    a: "2",
+    b: "22",
+    c: "222",
+    d: "3",
+    e: "33",
+    f: "333",
+    g: "4",
+    h: "44",
+    i: "444",
+    j: "5",
+    k: "55",
+    l: "555",
+    m: "6",
+    n: "66",
+    o: "666",
+    p: "7",
+    q: "77",
+    r: "777",
+    s: "7777",
+    t: "8",
+    u: "88",
+    v: "888",
+    w: "9",
+    x: "99",
+    y: "999",
+    z: "9999",
+  };
+
+  const splitCode = value.split("-");
+  console.log(splitCode);
+
+  const mappedArrayCode = splitCode.map((code) => code);
+  return;
+};
+
+console.log(t9Keyboard("6-666-88-777-33-3-33-888"));
